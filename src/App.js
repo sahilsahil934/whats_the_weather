63edc9cecf8868f69b3190209d4864d343dffbd6
Sahil Verma <iamsahil1910@gmail.com>
2019-03-06 19:16:21 +0530

    Build the base of the app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import Main from './app/index';
+
+class App extends Component {
+  render() {
+    return (
+
+    <div className="App">
+      <Main />
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/app/index.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./style.css";
+// import { createSecureContext } from "tls";
+
+class Weather extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            location: "",
+            place: "",
+            temp: "",
+            weather: ""
+        };
+    }
+
+    render() {
+
+        return (
+            <div className="weather">
+                <label htmlFor="text">Enter Location</label>
+                <br />
+                <div id="location">
+                    <input onChange={this.changeValue} type="text" value={this.state.location} />
+                </div>
+                <div className="button">
+                    <button onClick={this.getWeather}>Check Weather</button>
+                </div>
+                <div>
+                    <h1>Location: {this.state.place}</h1>
+                    <h3>Temperature: {this.state.temp} C</h3>
+                    <h3>Condition: {this.state.weather}</h3>
+                </div>
+            </div>
+        );
+    }
+
+    changeValue = (event) => {
+
+        this.setState({
+            location: event.target.value
+        });
+    }
+
+    getWeather = () => {
+
+        fetch(`https://api.apixu.com/v1/current.json?key=e030e73c813d42038df30529190603&q=${this.state.location}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    place: data.location.name,
+                    temp: data.current.temp_c,
+                    weather: data.current.condition.text
+                });
+            });
+
+    }
+}
+
+export default class Main extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+
+        return (
+            <div className="main">
+                <div className="title">What's the Weather?</div>
+                <hr />
+                <Weather />
+            </div>
+        );
+    }
+}
+
