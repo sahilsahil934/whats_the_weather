92e00965b3f389099c9846fac5afc6b9cf97bc99
Sahil Verma <iamsahil1910@gmail.com>
2019-03-06 19:14:36 +0530

    Updates

---
+++ precache-manifest.b2e1f85d23cde180ffbc799ae32836c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b10c1aa90b033bf8a7f",
+    "url": "/whats_the_weather/static/js/runtime~main.5b10c1aa.js"
+  },
+  {
+    "revision": "4b59652eedc55b1fcc61",
+    "url": "/whats_the_weather/static/js/main.4b59652e.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/whats_the_weather/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "4b59652eedc55b1fcc61",
+    "url": "/whats_the_weather/static/css/main.39ea8139.chunk.css"
+  },
+  {
+    "revision": "3360358e7896dea9f328fc809103e9f7",
+    "url": "/whats_the_weather/index.html"
+  }
+];
